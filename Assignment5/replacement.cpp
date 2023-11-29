2e832ee32caefc2d208c585ba81020ae4ae91632
Kenny Liu <143909151+KennyLiu00@users.noreply.github.com>
2023-11-28 17:19:55 -0800

    Update replacement.cpp

--- Assignment5/replacement.cpp
+++ Assignment5/replacement.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file replacement.cpp
- * @author Kyrstn Hall and EJ Lilagan
+ * @author Kenny Liu & Jesus Mendoza
  * @brief A base class for different page replacement algorithms.
  * @version 0.1
  */
@@ -10,13 +10,11 @@
 #include <iostream>
 #include "replacement.h"
 
-// TODO: Add your implementation of the Replacement member functions here
 
 // Constructor
 Replacement::Replacement(int num_pages, int num_frames)
 : page_table(num_pages)
 {
-	//TODO: Add your implementation here
   total_pages = num_pages;
   total_frames = num_frames;
 
@@ -31,14 +29,12 @@ Replacement::Replacement(int num_pages, int num_frames)
 // Destructor
 Replacement::~Replacement()
 {
-  // TOOD: Add your code here
 }
 
 // Simulate a single page access 
 // @return true if it's a page fault
 bool Replacement::access_page(int page_num, bool is_write)
 {
-  // TODO: Add your implementation here
   
   // If the page is valid, it calls the touch_page function. 1
   // If the page is not valid but free frames are available, it calls the load_page function. 2
@@ -65,7 +61,6 @@ bool Replacement::access_page(int page_num, bool is_write)
   
 // Print out statistics of simulation
 void Replacement::print_statistics() const {
-  // TODO: print out the number of references, number of page faults and number of page replacements
 	std::cout << "Number of references: \t\t"  << references << std::endl;
 	std::cout << "Number of page faults: \t\t" << page_faults << std::endl;
 	std::cout << "Number of page replacements: \t"  << page_replacement << std::endl;
